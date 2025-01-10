ee486a11e749e9153cc8a79a222c58b334be0c5e
cbandara-ifs <chinthaka.sci.se@gmail.com>
2025-01-10 20:44:15 +1100

    changing author class functional components

--- src/components/authors/AuthorsPage.js
+++ src/components/authors/AuthorsPage.js
@@ -1,46 +1,24 @@
-import React from "react";
-import { connect } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import * as authorActions from "../../redux/actions/authorActions";
-import PropTypes from "prop-types";
-import { bindActionCreators } from "redux";
 
-class AuthorsPage extends React.Component {
-  componentDidMount() {
-    const { actions, authors } = this.props;
+export default function AuthorsPage() {
+  const dispatch = useDispatch();
+  const authors = useSelector(state => state.authors);
 
+  useEffect(() => {
     if (authors.length === 0) {
-      actions.loadAuthors().catch(error => {
+      dispatch(authorActions.loadAuthors()).catch(error => {
         alert("loading courses failed " + error);
       });
     }
-  }
+  }, [dispatch, authors]);
 
-  render() {
-    return (
-      <>
-        {this.props.authors.map(author => (
-          <div key={author.firstName}>{author.firstName}</div>
-        ))}
-      </>
-    );
-  }
+  return (
+    <>
+      {authors.map(author => (
+        <div key={author.firstName}>{author.firstName}</div>
+      ))}
+    </>
+  );
 }
-
-AuthorsPage.propTypes = {
-  authors: PropTypes.array.isRequired,
-  actions: PropTypes.object.isRequired
-};
-
-function mapStateToProps(state) {
-  return {
-    authors: state.authors
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    actions: bindActionCreators(authorActions, dispatch)
-  };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(AuthorsPage);
